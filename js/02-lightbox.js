55bd645a56fe47757f3c40644884554d14fbaad3
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2022-11-10 11:37:24 +0200

    SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const basicBox = document.querySelector(".gallery");
+
+function createGallery(elements) {
+  return elements
+    .map(
+      (element) =>
+        `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`
+    )
+    .join("");
+}
+
+basicBox.innerHTML = createGallery(galleryItems);
+
+const gallery = new SimpleLightbox("ul.gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+console.log(gallery);
